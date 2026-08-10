0a26072355895ee28c5a1202152be6359911f1c1
Debjit369 <debjitkanjilal41@gmail.com>
2026-08-11 02:34:42 +0530

    3

---
+++ frontend/src/test-imports.ts
@@ -0,0 +1,11 @@
+import {
+  StellarWalletsKit,
+  WalletNetwork,
+  FreighterModule,
+  AlbedoModule,
+  xBullModule,
+  RabetModule,
+  HanaModule
+} from '@creit.tech/stellar-wallets-kit';
+
+console.log('Imports succeeded!', StellarWalletsKit, WalletNetwork, FreighterModule, AlbedoModule, xBullModule, RabetModule, HanaModule);
